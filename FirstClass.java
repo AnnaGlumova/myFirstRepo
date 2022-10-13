7968ab98d12e9f7bc7851b63100f7d5bf36886db
Anna Glumova <annetta.glumova@gmail.com>
2022-10-13 23:21:00 +0700

    New_Java_class

--- FirstClass.java
+++ FirstClass.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class FirstClass {
+public class FirstClass {
+
+    public static void main(String[] args) {
+        String a ="Hello, world!";
+        System.out.println(a);
+    }
 }
